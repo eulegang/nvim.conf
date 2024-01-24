9ad8d88963df3724695d6c201e2ca6bce125c73b
eulegang <eulegang@eulegang.dev>
2024-01-23 23:13:55 -0600

    Adding lsp formatting

--- init.lua
+++ init.lua
@@ -5,25 +5,23 @@ require('lazy').setup("eulegang.lazy")
 
 local lspconfig = require('lspconfig')
 lspconfig.lua_ls.setup({
-  settings = {
-    Lua = {
-      completion = {
-        callSnippet = "Replace"
-      }
-    }
-  }
+	settings = {
+		Lua = {
+			completion = {
+				callSnippet = "Replace"
+			}
+		}
+	}
 })
 
 lspconfig.rust_analyzer.setup {
-  -- Server-specific settings. See `:help lspconfig-setup`
-  settings = {
-    ['rust-analyzer'] = {},
-  },
-    diagnostics = {
-      globals = {
-        "vim"
-      }
-    },
+	-- Server-specific settings. See `:help lspconfig-setup`
+	settings = {
+		['rust-analyzer'] = {},
+	},
+	diagnostics = {
+		globals = {
+			"vim"
+		}
+	},
 }
-
-
