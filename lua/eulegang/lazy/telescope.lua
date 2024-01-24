ffca958dce6a60f6e1b3774a8b6873049765c1d4
eulegang <eulegang@eulegang.dev>
2024-01-23 20:21:26 -0600

    Adding more modules

--- lua/eulegang/lazy/telescope.lua
+++ lua/eulegang/lazy/telescope.lua
@@ -1,12 +1,16 @@
 return {
-    'nvim-telescope/telescope.nvim', 
-    tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-	require('telescope').setup({ })
-	local builtin = require('telescope.builtin')
+	{
+	    'nvim-telescope/telescope.nvim', 
+	    tag = '0.1.5',
+	    dependencies = { 'nvim-lua/plenary.nvim' },
+	    config = function()
+		require('telescope').setup({ })
+		local builtin = require('telescope.builtin')
 
-	vim.keymap.set('n', '<c-p>', builtin.find_files, { })
-	vim.keymap.set('n', '<c-g>', builtin.live_grep, { })
-    end
+		vim.keymap.set('n', '<c-p>', builtin.find_files, { })
+		vim.keymap.set('n', '<c-g>', builtin.live_grep, { })
+	    end
+	},
+
+	'nvim-telescope/telescope-ui-select.nvim'
 }
