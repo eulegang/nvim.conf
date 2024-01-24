ed928451fc3ad27f30b793140e8e68428d7e123d
eulegang <eulegang@eulegang.dev>
2024-01-23 22:07:42 -0600

    Adding lsp configs

--- init.lua
+++ init.lua
@@ -3,4 +3,27 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup("eulegang.lazy")
 
+local lspconfig = require('lspconfig')
+lspconfig.lua_ls.setup({
+  settings = {
+    Lua = {
+      completion = {
+        callSnippet = "Replace"
+      }
+    }
+  }
+})
+
+lspconfig.rust_analyzer.setup {
+  -- Server-specific settings. See `:help lspconfig-setup`
+  settings = {
+    ['rust-analyzer'] = {},
+  },
+    diagnostics = {
+      globals = {
+        "vim"
+      }
+    },
+}
+
 
