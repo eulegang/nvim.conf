58994b52368012da2ec207f2e3f74073e03ca0ff
eulegang <eulegang@eulegang.dev>
2024-01-24 17:09:56 -0600

    fixing telescope-select-ui

--- lua/eulegang/lazy/telescope.lua
+++ lua/eulegang/lazy/telescope.lua
@@ -4,13 +4,22 @@ return {
 		tag = '0.1.5',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
-			require('telescope').setup({})
+			require('telescope').setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown {
+						}
+					}
+				}
+			})
 			local builtin = require('telescope.builtin')
 
 			vim.keymap.set('n', '<c-p>', builtin.find_files, {})
 			vim.keymap.set('n', '<c-g>', builtin.live_grep, {})
+
+			require("telescope").load_extension("ui-select")
 		end
 	},
 
-	'nvim-telescope/telescope-ui-select.nvim'
+	{ 'nvim-telescope/telescope-ui-select.nvim', opts = {} }
 }
