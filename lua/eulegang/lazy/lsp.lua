094ae5b1e1476ac5b8fc96b9daa2350352116335
eulegang <eulegang@eulegang.dev>
2024-01-23 23:28:07 -0600

    Fixing some lsp keybindings

--- lua/eulegang/lazy/lsp.lua
+++ lua/eulegang/lazy/lsp.lua
@@ -11,9 +11,13 @@ return {
 			-- Global mappings.
 			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
 			vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-			vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-			vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-			vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+			vim.keymap.set('n', '<space>q', vim.diagnostic.setqflist)
+
+			vim.keymap.set('n', '<c-k>', ':cprev<cr>')
+			vim.keymap.set('n', '<c-j>', ':cnext<cr>')
+
+			vim.keymap.set('n', 'gk', vim.diagnostic.goto_prev)
+			vim.keymap.set('n', 'gj', vim.diagnostic.goto_next)
 
 			-- Use LspAttach autocommand to only map the following keys
 			-- after the language server attaches to the current buffer
@@ -30,7 +34,6 @@ return {
 					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 					vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 					vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-					vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
 					vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
 					vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
 					vim.keymap.set('n', '<space>wl', function()
