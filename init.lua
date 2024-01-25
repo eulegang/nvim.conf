b98104f64cf498f035ed388a22a8923f9198e52c
eulegang <eulegang@eulegang.dev>
2024-01-25 17:55:10 -0600

    Adding some options

--- init.lua
+++ init.lua
@@ -33,5 +33,8 @@ lspconfig.rust_analyzer.setup {
 vim.o.wrap = false
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
 
 vim.o.clipboard = 'unnamedplus'
