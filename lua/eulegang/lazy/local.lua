768c949bc6ecb9bdb6134e92ea2670a3be295a8d
eulegang <eulegang@eulegang.dev>
2024-01-27 13:16:57 -0600

    Adding atkpx config

---
+++ lua/eulegang/lazy/local.lua
@@ -0,0 +1,10 @@
+return {
+    "atkpx.nvim",
+    dir = "/home/eulegang/proj/atkpx.nvim",
+
+    opts = {
+        debug = "/home/eulegang/proj/atkpx/",
+        file = "/home/eulegang/proj/atkpx/init.lua",
+        log = "/home/eulegang/.local/state/nvim/atkpx.log",
+    }
+}
